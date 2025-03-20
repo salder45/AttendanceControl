d3a84f0d1badf99424408919679fe4bbe7ce0fc5
Eder Martinez <edermtz.04@gmail.com>
2025-03-19 21:52:01 -0600

    Add new Spring Boot project structure

---
+++ src/main/java/mx/edermtz/AttendanceControl/AttendanceControlApplication.java
@@ -0,0 +1,13 @@
+package mx.edermtz.AttendanceControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edermtz/AttendanceControl/AttendanceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edermtz.AttendanceControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
